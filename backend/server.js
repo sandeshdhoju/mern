07929c2af4bafb15662861c07b2b7bfa54588e31
Sandesh Dhoju <sandeshdhoju@gmail.com>
2023-05-02 23:04:51 +1000

    Added frontend UI

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 6000
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const colors = require('colors')
 const connectDB = require('./config/db')
 
@@ -13,6 +13,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
 
 app.use(errorHandler)
 
