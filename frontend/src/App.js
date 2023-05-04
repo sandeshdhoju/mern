6703101b057a41fb50a081c51e949ec58b37ff40
Sandesh Dhoju <sandeshdhoju@gmail.com>
2023-05-04 16:56:53 +1000

    Created Goal Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
 
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
@@ -18,6 +19,7 @@ function App() {
       </Routes>
     </div>
     </Router>
+    <ToastContainer />
     </>
   );
 }
